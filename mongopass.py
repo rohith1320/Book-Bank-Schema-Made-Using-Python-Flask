e6db3e3cabd64f09b6a2b1ce17b6bbac84fdea92
Rohith Muralidhar <53670816+rohith1320@users.noreply.github.com>
2022-06-04 10:57:42 +0530

    Add files via upload

---
+++ mongopass.py
@@ -0,0 +1,12 @@
+
+# from bson.json_util import dumps
+# import json
+#mongopass ="mongodb+srv://admin:admin@cluster0.mdvqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+#mongopass="mongodb+srv://admin:admin@cluster0.mdvqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+# db = pymongo.database.Database(client, 'mydatabase')
+# col = pymongo.collection.Collection(db, 'mycollection')
+# col_results = json.loads(dumps(col.find().limit(5).sort("time", -1)))
+
+mongopass="mongodb+srv://admin:admin@bbs.7if0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
